eed1f2e7ef2d758061dbcf9c2267c70f81b54d3b
Gonzalo Volonterio <volonterio.gonzalo1987@gmail.com>
2023-04-24 20:14:30 -0300

    update

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "OnlyFansCloneApp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "OnlyFansCloneApp": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3onlyfanscloneappstorageb3e1fac4": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
